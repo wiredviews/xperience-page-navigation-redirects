89600ca1efcbc131d45533e65bf445cf377f4b63
Seany Computer <sean@wiredviews.com>
2021-09-21 02:05:05 -0400

    feat(sln): initial code/project setup

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# xperience-page-navigation-redirects
+# Xperience Page Navigation Redirects
+
+[![NuGet Package](https://img.shields.io/nuget/v/XperienceCommunity.PageNavigationRedirects.svg)](https://www.nuget.org/packages/XperienceCommunity.PageNavigationRedirects)
+
 An ASP.NET Core ResourceFilter that can redirect HTTP requests to other URLs, configurable from the Xperience Administration application
+
+## Dependencies
+
+This package is compatible with Kentico Xperience 13 ASP.NET Core applications.
+
+This package should be used in combination with the [XperienceCommunity.PageCustomDataControlExtender](https://github.com/wiredviews/xperience-page-custom-data-control-extender)
+
+## How to Use?
+
+1. First, install the NuGet package in your Kentico Xperience live site (Content Delivery) ASP.NET Core project
+
+   ```bash
+   dotnet add package XperienceCommunity.PageNavigationRedirects
+   ```
+
+## How Does It Work?
+
+## References
+
+### Kentico Xperience
+
+- [Field Editor: Creating New Fields](https://docs.xperience.io/custom-development/extending-the-administration-interface/developing-form-controls/reference-field-editor#ReferenceFieldeditor-Creatingnewfields)
+- [Inheriting from existing form controls](https://docs.xperience.io/custom-development/extending-the-administration-interface/developing-form-controls/inheriting-from-existing-form-controls)
+- [Defining form control parameters](https://docs.xperience.io/custom-development/extending-the-administration-interface/developing-form-controls/defining-form-control-parameters)
+- [XperienceCommunity.PageCustomDataControlExtender](https://github.com/wiredviews/xperience-page-custom-data-control-extender)
---
+++ src/XperienceCommunity.PageNavigationRedirects/PageCustomDataRedirectResourceFilter.cs
@@ -0,0 +1,118 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using CMS.DocumentEngine;
+using Kentico.Content.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace XperienceCommunity.PageNavigationRedirects
+{
+    /// <summary>
+    /// Redirects the current request to another URL based on the <see cref="PageExtensions.RedirectionType(TreeNode)"/> value
+    /// </summary>
+    public class PageCustomDataRedirectResourceFilter : IAsyncResourceFilter
+    {
+        private readonly IPageDataContextRetriever contextRetriever;
+        private readonly IPageUrlRetriever urlRetriever;
+        private readonly IPageRetriever pageRetriever;
+
+        public PageCustomDataRedirectResourceFilter(
+            IPageDataContextRetriever contextRetriever,
+            IPageUrlRetriever urlRetriever,
+            IPageRetriever pageRetriever)
+        {
+            this.contextRetriever = contextRetriever;
+            this.urlRetriever = urlRetriever;
+            this.pageRetriever = pageRetriever;
+        }
+
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            if (!contextRetriever.TryRetrieve<TreeNode>(out var data))
+            {
+                return;
+            }
+
+            var page = data.Page;
+
+            var redirectionType = page.RedirectionType();
+
+            if (redirectionType == PageRedirectionType.None)
+            {
+                return;
+            }
+
+            if (redirectionType == PageRedirectionType.External)
+            {
+                string? url = page.ExternalRedirectURL();
+
+                if (url is string)
+                {
+                    context.Result = new RedirectResult(url, false);
+                }
+
+                return;
+            }
+
+            if (redirectionType == PageRedirectionType.Internal)
+            {
+                var nodeGuid = page.InternalRedirectNodeGuid();
+
+                if (!nodeGuid.HasValue)
+                {
+                    return;
+                }
+
+                var pages = await pageRetriever.RetrieveAsync<TreeNode>(
+                    q => q.WhereEquals(nameof(TreeNode.NodeGUID), nodeGuid.Value),
+                    c => c.Key($"{nameof(PageCustomDataRedirectResourceFilter)}|{PageRedirectionType.Internal}|{nodeGuid.Value}"),
+                    cancellationToken: context.HttpContext.RequestAborted);
+
+                var linkedPage = pages.FirstOrDefault();
+
+                if (linkedPage is null)
+                {
+                    return;
+                }
+
+                var url = urlRetriever.Retrieve(linkedPage);
+
+                if (url is null)
+                {
+                    return;
+                }
+
+                context.Result = new RedirectResult(url.RelativePath, false);
+
+                return;
+            }
+
+            if (redirectionType == PageRedirectionType.FirstChild)
+            {
+                var children = await pageRetriever.RetrieveAsync<TreeNode>(
+                    q => q
+                        .WhereEquals(nameof(TreeNode.NodeParentID), page.NodeID)
+                        .OrderBy(nameof(TreeNode.NodeOrder))
+                        .TopN(1),
+                    c => c.Key($"{nameof(PageCustomDataRedirectResourceFilter)}|{PageRedirectionType.FirstChild}|{page.NodeID}"),
+                    cancellationToken: context.HttpContext.RequestAborted);
+
+                if (!children.Any())
+                {
+                    return;
+                }
+
+                var firstChild = children.Single();
+
+                var url = urlRetriever.Retrieve(firstChild);
+
+                if (url is null)
+                {
+                    return;
+                }
+
+                context.Result = new RedirectResult(url.RelativePath, false);
+            }
+        }
+    }
+}
